84fdaf8ba35807cbbe05003b905a410da1f52559
Wolfgang <Wolfgang@ProgrammingWithWolfgang.com>
2018-10-14 15:43:27 +0200

    init commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild BasicBot.sln -p:DeployOnBuild=true -p:PublishProfile=wolfgangchatbot-Web-Deploy.pubxml -p:Password=K8313CsllfeTvujjMgjSwT8z2BJuT4oEr3kJrChjLediGAcdhBQkRdxrfAp2
+
